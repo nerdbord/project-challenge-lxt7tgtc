7c238221674a62e30e434ebaf52bfc471633d4f6
transcendence12 <gosiek.krawczuk@gmail.com>
2024-07-01 15:49:32 +0200

    feat: file upload view, FormControl from chakra ui, image preview

--- src/ui/UploadImage.tsx
+++ src/ui/UploadImage.tsx
@@ -1,5 +1,7 @@
 import {
 	Input,
+	FormControl,
+	FormLabel,
 	Text,
 	Link,
 	Box,
@@ -7,7 +9,6 @@ import {
 	Flex,
 	Image,
 	HStack,
-	VStack,
 	Button,
 } from "@chakra-ui/react";
 
@@ -32,48 +33,120 @@ export const UploadImage = () => {
 								src="/upload.jpg"
 								alt="Upload box decoration image"
 							/>
-							<Text
-								as="u"
-								mt={4}
-								textTransform="uppercase"
-								color="teal.500"
-								fontWeight="semibold"
-								_hover={{ fontWeight: "bold" }}
-							>
-								<label htmlFor="browse" className="cursor-pointer">
+							<FormControl mt={4}>
+								<FormLabel
+									htmlFor="browse"
+									cursor="pointer"
+									color="teal.500"
+									textAlign="center"
+									fontWeight="semibold"
+									textTransform="uppercase"
+								>
 									Click or Drag & drop your image here
-									<Input type="file" accept=".png, .jpg, .jpeg" id="browse" name="file" hidden />
-								</label>
-							</Text>
+									<Input
+										type="file"
+										accept=".png, .jpg, .jpeg"
+										id="browse"
+										name="file"
+										hidden
+										// onChange={handleFileChange}
+									/>
+								</FormLabel>
+							</FormControl>
 							<Text as="sub" mt={4} color="gray.700" fontSize="8px">
 								*Acceptable file formats: JPG, PNG, JPEG
 							</Text>
 						</Center>
 					</Box>
-					<VStack width="40%" alignItems="flex-start" spacing={4} maxHeight="80vh" overflowY="auto">
-						<Box
+					<Flex width="20%" alignItems="center" maxHeight="80vh">
+						<Flex
 							// key={index}
 							width="full"
 							padding="2"
-							backgroundColor="gray.100"
+							backgroundColor="gray.50"
 							borderRadius="md"
 							boxShadow="md"
+							direction="column"
+							gap="2"
 						>
-							<HStack width="full" justifyContent="space-between">
-								<Link color="teal.500" flex="1" isExternal isTruncated>
-									Some URL goes here
+							<HStack width="full" spacing={4} alignItems="center" justifyContent="space-around">
+								<Link isExternal>
+									<Image
+										alt=""
+										src="/logo.png"
+										boxSize="70px"
+										objectFit="cover"
+										border="black"
+										borderRadius="md"
+									/>
 								</Link>
-								<HStack spacing={2}>
-									<Button colorScheme="teal" size="sm">
-										Copy
-									</Button>
-									<Button bg="#ef4f4a" _hover={{ bg: "#861110" }} size="sm" color="white">
-										Delete
-									</Button>
-								</HStack>
+
+								<Button colorScheme="teal" size="sm">
+									Copy
+								</Button>
+								<Button bg="#ef4f4a" _hover={{ bg: "#861110" }} size="sm" color="white">
+									Delete
+								</Button>
 							</HStack>
-						</Box>
-					</VStack>
+							<HStack width="full" spacing={4} alignItems="center" justifyContent="space-around">
+								<Link isExternal>
+									<Image
+										alt=""
+										src="/upload.jpg"
+										boxSize="70px"
+										objectFit="cover"
+										border="black"
+										borderRadius="md"
+									/>
+								</Link>
+
+								<Button colorScheme="teal" size="sm">
+									Copy
+								</Button>
+								<Button bg="#ef4f4a" _hover={{ bg: "#861110" }} size="sm" color="white">
+									Delete
+								</Button>
+							</HStack>
+							<HStack width="full" spacing={4} alignItems="center" justifyContent="space-around">
+								<Link isExternal>
+									<Image
+										alt=""
+										src="/logo.png"
+										boxSize="70px"
+										objectFit="cover"
+										border="black"
+										borderRadius="md"
+									/>
+								</Link>
+
+								<Button colorScheme="teal" size="sm">
+									Copy
+								</Button>
+								<Button bg="#ef4f4a" _hover={{ bg: "#861110" }} size="sm" color="white">
+									Delete
+								</Button>
+							</HStack>
+							<HStack width="full" spacing={4} alignItems="center" justifyContent="space-around">
+								<Link isExternal>
+									<Image
+										alt=""
+										src="/upload.jpg"
+										boxSize="70px"
+										objectFit="cover"
+										border="black"
+										borderRadius="md"
+									/>
+								</Link>
+
+								<Button colorScheme="teal" size="sm">
+									Copy
+								</Button>
+								<Button bg="#ef4f4a" _hover={{ bg: "#861110" }} size="sm" color="white">
+									Delete
+								</Button>
+							</HStack>
+						</Flex>
+					</Flex>
 				</HStack>
 			</Flex>
 		</Center>
